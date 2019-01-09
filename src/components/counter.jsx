557a8190753689f777604935da96c4bc27a3b197
tim-ilovetheory <tim@ilovetheory.com>
2019-01-09 16:55:29 -0500

    Props vs State tutorial

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,30 +2,36 @@ import React, { Component } from 'react';
 
 class Counter extends Component { 
     state = {
-        count: 0,
+        value: this.props.value
     };
     
-    handleIncrement() {
-        console.log("Increment clicked", this);
-    }
+    // constructor() {
+    //     super();
+    //     this.handleIncrement = this.handleIncrement.bind(this);
+    // }
+
+    handleIncrement = () => {
+        this.setState({value: this.state.value + 1});
+    };
 
     render() { 
+
         return ( 
             <div>
                 <span class={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={this.handleIncrement} class="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={ () => this.handleIncrement} class="btn btn-secondary btn-sm">Increment</button>
             </div> 
         );
     }
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.state.value === 0) ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const { count } = this.state;
+        const {value: count} = this.state;
         return count === 0 ? "Zero" : count;
     }
 }
