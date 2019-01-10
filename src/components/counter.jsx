42165502864b490f4c1c170e74468922c85c80e2
tim-ilovetheory <tim@ilovetheory.com>
2019-01-10 16:43:54 -0500

    React intro tutorial finished

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,38 +1,39 @@
 import React, { Component } from 'react';
 
 class Counter extends Component { 
-    state = {
-        value: this.props.value
-    };
-    
-    // constructor() {
-    //     super();
-    //     this.handleIncrement = this.handleIncrement.bind(this);
-    // }
+    componentDidUpdate(prevProps, prevState) {
+        console.log('prevProps', prevProps);
+        console.log('prevState', prevState);
+        if(prevProps.counter.value !== this.props.counter.value) {
+            // Ajax call and get new data from the server
+        }
+    }
 
-    handleIncrement = () => {
-        this.setState({value: this.state.value + 1});
-    };
+    componentWillUnmount() {
+        console.log('Counter - unmount');
+    }
 
     render() { 
+        console.log('Counter - rendered');
 
         return ( 
             <div>
                 <span class={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={ () => this.handleIncrement} class="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger m-2">Delete</button>
             </div> 
         );
     }
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.value === 0) ? "warning" : "primary";
+        classes += this.props.counter === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const {value: count} = this.state;
-        return count === 0 ? "Zero" : count;
+        const {value} = this.props.counter;
+        return value === 0 ? "Zero" : value;
     }
 }
 
---
+++ src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';   
+
+// Stateless functional component
+const NavBar = ({totalCounters}) => {
+    console.log('Navbar - rendered');
+
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand" href="#">
+                Navbar {" "}
+                <span className="badge badge-pill badge-secondary">
+                {totalCounters}
+                </span>
+            </a>
+        </nav>
+    );
+};
+ 
+export default NavBar;
